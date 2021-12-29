fbc9f417f242016520e358787eabe83a2658a010
Max Playle <maxplayle04@gmail.com>
2021-12-29 01:30:34 +0000

    Add project files.

---
+++ Section136Callouts.UpdateAnnouncer.Net6/Program.cs
@@ -0,0 +1,111 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using Section136Callouts.UpdateAnnouncer.Net6;
+using System.Net;
+using System.Net.Http.Json;
+using System.Reflection.Metadata.Ecma335;
+using System.Security;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Hello, World!");
+
+//WebhookHelper.Instance.SendUpdate("1", "1", out string? webhookFailed);
+
+// *******************************************************************
+// Setup the application, and set all security protocols as required.
+// *******************************************************************
+
+ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+HttpClient client = new();
+
+while (true)
+{
+    Tick();
+    Thread.Sleep(TimeSpan.FromMinutes(5));
+}
+
+void Tick()
+{
+    var version = GetLastVersion().GetAwaiter();
+}
+
+void printColoured(string msg, ConsoleColor color)
+{
+    ConsoleColor c = Console.ForegroundColor;
+    Console.ForegroundColor = color;
+    Console.WriteLine(msg);
+    Console.ForegroundColor = c;
+}
+
+void print(string msg, params object?[]? args) => Console.WriteLine(msg, args);
+
+async Task GetLastVersion()
+{
+    try
+    {
+        var r = await client.GetAsync("http://www.lcpdfr.com/applications/downloadsng/interface/api.php?do=checkForUpdates&fileId=30221");
+
+        dynamic returnedJson = JsonConvert.DeserializeObject(value: r.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+
+        Console.WriteLine(returnedJson);
+
+        if (returnedJson.error != null && !string.IsNullOrEmpty(returnedJson.error.ToString()))
+        {
+            throw new Exception(returnedJson.error);
+        }
+        else
+            Console.WriteLine("[OK] No errors detected whilst obtaining versioning information.");
+
+        string fullVersionString = returnedJson.version.ToString();
+
+        Console.WriteLine("Version number: {0}", returnedJson.version.ToString());
+
+        Regex pattern = new Regex(@"\d+(\.\d+)+");
+        Match m = pattern.Match(fullVersionString);
+        string version = m.Value;
+
+        Console.WriteLine("Regex returned version number: {0}", version);
+
+        if (string.IsNullOrEmpty(KnownValues.Default.LastKnownUpdate))
+        {
+            printColoured("No version information is stored.", ConsoleColor.Red);
+            Console.WriteLine("* Storing new data...");
+            KnownValues.Default.LastKnownUpdate = version;
+            KnownValues.Default.LastUpdateCheck = DateTime.Now;
+            KnownValues.Default.Save();
+        }
+        else
+            printColoured("App has stored version information", ConsoleColor.Green);
+
+        bool canParseVersion = Version.TryParse(version, out Version serverVersion);
+        bool canParseKnownVersion = Version.TryParse(version, out Version storedVersion);
+
+        if (canParseVersion && canParseKnownVersion && serverVersion != null && storedVersion != null)
+        {
+            if (serverVersion > storedVersion)
+            {
+                printColoured("new update detected!", ConsoleColor.Green);
+                print("Stored: {0}\nServer: {1}", storedVersion, serverVersion);
+                WebhookHelper.Instance.SendUpdate(serverVersion.ToString(), fullVersionString, out string failReason);
+                KnownValues.Default.LastKnownUpdate = serverVersion.ToString();
+            }
+            else
+                print("No new version detected. (Received version info: {0})", serverVersion.ToString());
+            KnownValues.Default.LastUpdateCheck = DateTime.Now;
+            KnownValues.Default.Save();
+        }
+        else
+        {
+            printColoured("PARSE ERROR", ConsoleColor.Red);
+            print("local parsed? {0}, server parsed? {1}", canParseKnownVersion, canParseVersion);
+        }
+        return;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Could not fetch most recent version.");
+        Console.WriteLine(e);
+    }
+}
+
---
+++ Section136Callouts.UpdateAnnouncer.Net6/WebhookHelper.cs
@@ -0,0 +1,48 @@
+﻿using Discord.Webhooks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section136Callouts.UpdateAnnouncer.Net6
+{
+    internal class WebhookHelper
+    {
+
+        static WebhookHelper i;
+        internal static WebhookHelper Instance => i ?? (i = new WebhookHelper());
+
+        private Webhook webhook;
+        
+
+        public WebhookHelper()
+        {
+            webhook = new Webhook()
+            {
+                WebhookUrl = "https://discord.com/api/webhooks/925541212344184943/xHPSO0EKvuLFA8QLy94Z-o_j_0Dp-n49M70t0hlRnTuEW-pW6IZV5KL1VHNOaY5b88KN",
+                Username = "Section136Callouts Update Detector",
+                Message = "A new update to Section136Callouts has been detected"
+            };
+        }
+
+        public bool SendUpdate(string newVersion, string fullVersionName, out string? failReason)
+        {
+            Console.WriteLine("[WEBHOOK] Sending update to webhook.");
+
+            failReason = null;
+
+            if (webhook is null)
+            {
+                failReason = "The webhook is not setup.";
+                return false;
+            }
+
+            webhook.Message = string.Format("**NEW UPDATE DETECETD!**\n\nSection136Callouts version **{0}** has been released on the LCPDFR website!\n\nYou can download it at https://section136.maxplayledev.org  \n\nVersion information: {1}\nRelease timestamp: Within the last 5 minutes", newVersion, fullVersionName);
+
+            webhook.SendInstance();
+
+            return true;
+        }
+    }
+}
